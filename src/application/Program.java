f63d29e6fb5178bd87593daf766c1bd7c28972c0
Madson Santos <madsonsamsung@gmail.com>
2020-04-15 14:54:13 -0300

    Implementando insert da classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -13,22 +14,27 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("Teste 01: findById classe SellerDaoJDBC");
+		System.out.println("Teste 01: findById da classe SellerDaoJDBC");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\nTeste 02: findByDepartment classe SellerDaoJDBC");
+		System.out.println("\nTeste 02: findByDepartment da classe SellerDaoJDBC");
 		List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
 		
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\nTeste 03: findAll classe SellerDaoJDBC");
+		System.out.println("\nTeste 03: findAll da classe SellerDaoJDBC");
 		list = sellerDao.findAll();
 		
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\nTeste 04: insert da classe SellerDaoJDBC");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, new Department(2, null));
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 }
