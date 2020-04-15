08c67efbeea45b29bcf977bfc13e1a048fb83878
Madson Santos <madsonsamsung@gmail.com>
2020-04-15 09:11:34 -0300

    Criação das classes Department e Seller

---
+++ src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+import db.DB;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		Statement st = null;
+		
+		try {
+			
+			conn = DB.getConnection();
+			st = conn.createStatement();
+			
+			Department obj = new Department(1, "books");
+			Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+			
+			System.out.println(seller);
+			
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+}
