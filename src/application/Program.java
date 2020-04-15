d02f20149058c3fbbbce4c878a8361140b660ea7
Madson Santos <madsonsamsung@gmail.com>
2020-04-15 13:34:37 -0300

    Implementando findByDepartment na classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -13,6 +16,12 @@ public class Program {
 		System.out.println("Test 01: findById class SellerDaoJDBC");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-
+		
+		System.out.println("\nTest 02: findByDepartment class SellerDaoJDBC");
+		List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+		
+		for (Seller obj: list) {
+			System.out.println(obj);
+		}
 	}
 }
