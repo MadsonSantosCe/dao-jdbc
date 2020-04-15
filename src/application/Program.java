6750e8b0ffbad7a7671844755d1d2118afcd480c
Madson Santos <madsonsamsung@gmail.com>
2020-04-15 10:44:21 -0300

    Reutilizando a instanciação da classe Seller e Department na classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,6 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("Test 01: findById class SellerDaoJDBC");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
